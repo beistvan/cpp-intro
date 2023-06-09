057963cbad400432eed8eb78fb7dc268a9bb4f3d
Beregszászi István <beistvan@gmail.com>
2023-06-10 00:28:32 +0200

    School Desks

---
+++ school-desks.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << a / 2 + a % 2 + b / 2 + b % 2 + c / 2 + c % 2;
+    return 0;
+}
