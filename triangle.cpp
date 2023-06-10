e17100f28378a8fac73eeb118c0091b6cf7823ff
Beregszászi István <beistvan@gmail.com>
2023-06-10 07:48:10 +0200

    Triangle

---
+++ triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b) {
+		cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+}
