a02ebd6c08693fd7b29ec2f254502b0d4af26552
Beregszászi István <beistvan@gmail.com>
2023-06-27 07:44:44 +0200

    Set Of Linear Equations

---
+++ set-of-linear-equations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double a1, b1, c1, a2, b2, c2, x1, x2, d;
+    cin >> a1 >> b1 >> a2 >> b2 >> c1 >> c2;
+    d = a1 * b2 - a2 * b1; 
+    x1 = (c1 * b2 - c2 * b1) / d;
+    x2 = (c2 * a1 - c1 * a2) / d;
+    cout << x1 << " " << x2;
+    return 0;
+}
