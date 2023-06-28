76a1a1d180e0f719a7a66a9fbec1d668d804d723
Beregszászi István <beistvan@gmail.com>
2023-06-28 07:41:45 +0200

    Count Positive Elements

---
+++ count-positives.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, c = 0;
+    cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0) {
+            c++;
+        }
+    }
+    cout << c;
+    return 0;
+}
