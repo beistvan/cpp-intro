fbc187933071c0506f77835f03950e7bf5566f05
Beregszászi István <beistvan@gmail.com>
2023-06-29 07:50:45 +0200

    Neighbors Of Same Sign

---
+++ neighbors-same-signs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 1; i < n; i++) {
+        if ((a[i] * a[i - 1]) > 0) {
+            if (a[i - 1] < a[i])
+            {
+                cout << a[i - 1] << " " << a[i];
+            }
+            else
+            {
+                cout << a[i] << " " << a[i - 1];
+            }
+            break;
+        }
+    }
+    return 0;
+}
