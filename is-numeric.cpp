507e366dd99fed09d9968517c09b89162057b91a
Beregszászi István <beistvan@gmail.com>
2023-07-04 00:38:08 +0200

    Is Numeric

---
+++ is-numeric.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    for (auto c : s) {
+        if (c >= '0' && c <= '9') {
+            cout << "yes";
+        }
+        else {
+            cout << "no";
+        }
+    }
+    return  0;
+}
